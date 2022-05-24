b3c9e93e1a97268dd24322f608780ea82b473ce1
YePyieOo <yepyieoo96@gmail.com>
2022-05-25 01:16:20 +0630

    Testing homepage

--- 04.Development/Project/Customer/View/homepage.php
+++ 04.Development/Project/Customer/View/homepage.php
@@ -12,7 +12,7 @@
 </head>
 
 <body>
-   <nav class="navbar navbar-expand-lg navbar-light bg-danger">
+   <nav class="navbar navbar-expand-lg  nav-bg">
       <div class="container-fluid">
          <a class="navbar-brand" href="#">Navbar</a>
          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -24,29 +24,35 @@
                   <a class="nav-link active" aria-current="page" href="#">Home</a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
+                  <a class="nav-link" href="#">Search</a>
                </li>
                <li class="nav-item dropdown">
                   <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                     Dropdown
+                     Contact Us
                   </a>
                   <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                     <li><a class="dropdown-item" href="#">Action</a></li>
-                     <li><a class="dropdown-item" href="#">Another action</a></li>
-                     <li>
-                        <hr class="dropdown-divider">
-                     </li>
-                     <li><a class="dropdown-item" href="#">Something else here</a></li>
+                     <li><a class="dropdown-item fw-bold" href="#">Language</a></li>
+                     <li><a class="dropdown-item fw-bold" href="#">Education</a></li>
+                     <li><a class="dropdown-item fw-" href="#">Novels</a></li>
+                     <li><a class="dropdown-item fw-bold" href="#">Technology</a></li>
+                     <li><a class="dropdown-item fw-bold" href="#">Cartoons</a></li>
                   </ul>
                </li>
-               <li class="nav-item">
-                  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-               </li>
             </ul>
-            <form class="d-flex">
+            <!-- <form class="d-flex">
                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            </form> -->
+
+            <div class="d-flex">
+               <div class="">
+                  <ion-icon name="cart-outline"></ion-icon>
+                  <ion-icon name="person-outline"></ion-icon>
+                  <ion-icon name="moon-outline"></ion-icon>
+               </div>
+            </div>
+
+
          </div>
       </div>
    </nav>
