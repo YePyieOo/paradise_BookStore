dfcac0ac6016afa3396c85854f7a60096cb64e24
DESKTOP-KK6PVSK\user <nyimyomwa9@gmail.com>
2022-05-25 16:06:25 +0630

    Add Admin Nav Bar

--- 04.Development/Project/Admin/View/setting.php
+++ 04.Development/Project/Admin/View/setting.php
@@ -4,9 +4,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Setting</title>
+    <link rel="stylesheet" href="../resource/css/style.css">
+    <link rel="stylesheet" href="../resource/css/setting.css">
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <link rel="stylesheet" href="../resource/UI Library/bootstrap-5.0.2-dist/css/bootstrap.min.css" />
+    <script src="../resource/UI Library/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
+    <script src="../resource/js/setting.js"></script>
+    <script src="../resource/UI Library/jquery-3.3.1.min.js"></script>
 </head>
 <body>
-    
+    <div class="navbarbox">
+
+    <div class="buttonbox d-grid gap-2 col-2">
+        <div>
+            <div class="h4 text-light">Paradise Book</div>
+            <div class="h4 text-light">Store</div>
+        </div>
+        <button class="btn btn-light ">Dashboard</button>
+        <button class="btn btn-light  ">Book Info</button>
+        <button class="btn btn-light ">Author Info</button>
+        <button class="btn btn-light ">Shop Info</button>
+        <button class="btn btn-light ">User Info</button>
+        <button class="btn btn-light ">Order Info</button>
+        <button class="btn btn-light  ">Community</button>
+        <button class="btn btn-light  ">Setting</button>
+        <button class="btn btn-light ">Log Out</button>
+    </div>
+    </div>
 </body>
 </html>
