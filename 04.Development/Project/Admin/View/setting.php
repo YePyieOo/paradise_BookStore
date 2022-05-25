4bb8b3012789f23b93c7f284f373d38e936a1f5e
DESKTOP-KK6PVSK\user <nyimyomwa9@gmail.com>
2022-05-26 00:23:19 +0630

    Add

--- 04.Development/Project/Admin/View/setting.php
+++ 04.Development/Project/Admin/View/setting.php
@@ -1,48 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Setting</title>
-    <link rel="stylesheet" href="../resource/css/style.css">
-    <link rel="stylesheet" href="../resource/css/setting.css">
-    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <link rel="stylesheet" href="../resource/UI Library/bootstrap-5.0.2-dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../resource/css/style.css" />
+    <link rel="stylesheet" href="../resource/css/setting.css" />
     <script src="../resource/UI Library/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
     <script src="../resource/js/setting.js"></script>
     <script src="../resource/UI Library/jquery-3.3.1.min.js"></script>
 </head>
-
 <body>
-    <div class="container-fluid">
+<div class="container-fluid">
 
-        <div class="row">
-            <div class="col-2 buttonbox ">
-                <div class="h3 text-break mt-4 text-light">Paradise Book </div>
-                <div class="h3 text-light">Store</div>
-                <div class="btn-group-vertical container mb-4">
-                    <button type="button" class="btn btn-light text-break mt-3">Dashboard</button>
-                    <button type="button" class="btn btn-light text-break mt-3">Book Info</button>
-                    <button type="button" class="btn btn-light text-break mt-3">Author Info</button>
-                    <button type="button" class="btn btn-light text-break mt-3">Shop Info</button>
-                    <button type="button" class="btn btn-light text-break mt-3">User Info</button>
-                    <button type="button" class="btn btn-light text-break mt-3">Order Info</button>
-                    <button type="button" class="btn btn-light text-break mt-3">Community</button>
-                    <button type="button" class="btn btn-light text-break mt-3">Setting</button>
-                    <button type="button" class="btn btn-light text-break mt-3">Log Out</button>
-
-                </div>
-
-            </div>
-            <div class="col">
-
-            </div>
+<div class="row">
+    <div class="col-2 buttonbox ">
+        <div class="h3 text-break mt-4 text-light">Paradise Book </div>
+        <div class="h3 text-light">Store</div>
+        <div class="btn-group-vertical container mb-4">
+            <button type="button" class="btn btn-light text-break mt-3">Dashboard</button>
+            <button type="button" class="btn btn-light text-break mt-3">Book Info</button>
+            <button type="button" class="btn btn-light text-break mt-3">Author Info</button>
+            <button type="button" class="btn btn-light text-break mt-3">Shop Info</button>
+            <button type="button" class="btn btn-light text-break mt-3">User Info</button>
+            <button type="button" class="btn btn-light text-break mt-3">Order Info</button>
+            <button type="button" class="btn btn-light text-break mt-3">Community</button>
+            <button type="button" class="btn btn-light text-break mt-3">Setting</button>
+            <button type="button" class="btn btn-light text-break mt-3">Log Out</button>
 
         </div>
-    </div>
-</body>
 
+    </div>
+    <div class="col">
+
+    </div>
+
+</div>
+</div>
+</body>
 </html>
