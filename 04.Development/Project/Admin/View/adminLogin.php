a4ed49b27a5fb832785a785a61ae4df20ce18d61
Merries <merriesmmk5814@gmail.com>
2022-05-25 22:35:33 +0630

    update

---
+++ 04.Development/Project/Admin/View/adminLogin.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+
+<link rel="stylesheet" href="../resource/UI Library/bootstrap-5.0.2-dist/css/bootstrap.min.css" />
+<link rel="stylesheet" href="../resource/css/style.css" />
+<link rel="stylesheet" href="../resource/css/admin_login.css" />
+<body>
+   <div class="container">
+      <form action="/action_page.php">
+         <label for="fname">username:</label><br>
+         <input type="text" id="fname" value=""><br><br>
+         <label for="fname">password:</label><br>
+         <input type="password" id="fname" value=""><br><br>
+         <input type="submit" value="Submit">
+      </form>
+   </div>
+   <script src="../resource/UI Library/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
+   <script src="../resource/js/admin_login.js"></script>
+   <script src="../resource/UI Library/jquery-3.3.1.min.js"></script>
+</body>
+</html>
