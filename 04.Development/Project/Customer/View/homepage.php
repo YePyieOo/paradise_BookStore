9038cb2b0023b1ee996d5db949720c49c8850208
YePyieOo <yepyieoo96@gmail.com>
2022-05-25 12:30:09 +0630

    testing navbar

--- 04.Development/Project/Customer/View/homepage.php
+++ 04.Development/Project/Customer/View/homepage.php
@@ -12,12 +12,9 @@
 </head>
 
 <body>
-   <nav class="navbar navbar-expand-lg">
+   <nav class="navbar navbar-expand-lg navbar-light bg-light">
       <div class="container-fluid">
-         <a class="navbar-brand" href="#">
-            <img src="../resource/img/main-logo.png" alt="" width="30" height="24" class="d-inline-block align-text-top">
-            Paradise Book Store
-         </a>
+         <a class="navbar-brand" href="#">Navbar</a>
          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
          </button>
@@ -27,35 +24,29 @@
                   <a class="nav-link active" aria-current="page" href="#">Home</a>
                </li>
                <li class="nav-item">
-                  <a class="nav-link" href="#">Search</a>
+                  <a class="nav-link" href="#">Link</a>
                </li>
                <li class="nav-item dropdown">
-                  <a class="nav-link " href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                     Contact Us
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                     Dropdown
                   </a>
                   <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                     <li><a class="dropdown-item fw-bold" href="#">View Profile</a></li>
-                     <li><a class="dropdown-item fw-bold" href="#">Guide</a></li>
-                     <li><a class="dropdown-item fw-bold" href="#">FAQ's</a></li>
-                     <li><a class="dropdown-item fw-bold" href="#">Privacy Policy</a></li>
-                     <li><a class="dropdown-item fw-bold" href="#">Cartoons</a></li>
+                     <li><a class="dropdown-item" href="#">Action</a></li>
+                     <li><a class="dropdown-item" href="#">Another action</a></li>
+                     <li>
+                        <hr class="dropdown-divider">
+                     </li>
+                     <li><a class="dropdown-item" href="#">Something else here</a></li>
                   </ul>
                </li>
+               <li class="nav-item">
+                  <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+               </li>
             </ul>
-            <!-- <form class="d-flex">
+            <form class="d-flex">
                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form> -->
-
-            <div class="d-flex">
-               <div class="">
-                  <ion-icon name="cart-outline"></ion-icon>
-                  <ion-icon name="person-outline"></ion-icon>
-                  <ion-icon name="moon-outline"></ion-icon>
-               </div>
-            </div>
-
-
+            </form>
          </div>
       </div>
    </nav>
