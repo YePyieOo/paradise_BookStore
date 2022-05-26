b1d9bc2e25a382afcabe045187e8260a70cb34a3
Merries <merriesmmk5814@gmail.com>
2022-05-26 12:27:51 +0630

    adminLogin

--- 04.Development/Project/Admin/View/adminLogin.php
+++ 04.Development/Project/Admin/View/adminLogin.php
@@ -1,27 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Document</title>
-</head>
-
-<link rel="stylesheet" href="../resource/UI Library/bootstrap-5.0.2-dist/css/bootstrap.min.css" />
+   <title>Admin Login</title>
+   <link rel="stylesheet" href="../resource/UI Library/bootstrap-5.0.2-dist/css/bootstrap.min.css" />
 <link rel="stylesheet" href="../resource/css/style.css" />
-<link rel="stylesheet" href="../resource/css/admin_login.css" />
+<link rel="stylesheet" href="../resource/css/adminLogin.css" />
+<script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</head>
 <body>
-   <div class="container">
+   <div class="container mt-3">
+      <h2 class="text-center title">Admin Login</h2>
       <form action="/action_page.php">
-         <label for="fname">username:</label><br>
-         <input type="text" id="fname" value=""><br><br>
-         <label for="fname">password:</label><br>
-         <input type="password" id="fname" value=""><br><br>
-         <input type="submit" value="Submit">
+         <div class="mb-5 mt-4">
+            <input type="email" class="form-control" id=""placeholder="Enter username" name="username">
+         </div>
+         <div class="mb-5">
+            <input type="password" class="form-control" id=""placeholder="Enter password" name="pswd">
+         </div>
+         
+         <div class="d-grid">
+         <button type="button" class="btn btn-primary btn-block btn-lg fw-bold mt-3">Login</button>
+</div>
       </form>
    </div>
    <script src="../resource/UI Library/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
    <script src="../resource/js/admin_login.js"></script>
    <script src="../resource/UI Library/jquery-3.3.1.min.js"></script>
 </body>
+
 </html>
