2f11b35c53ec124fb163ae8d394034ed2267c49d
DESKTOP-KK6PVSK\user <nyimyomwa9@gmail.com>
2022-05-27 21:54:48 +0630

    Add edit Pages

---
+++ 04.Development/Project/Admin/View/Edit_privacypolicy.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Privacy Policy</title>
+    <link rel="stylesheet" href="../resource/UI Library/bootstrap-5.0.2-dist/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="../resource/css/style.css" />
+    <link rel="stylesheet" href="../resource/css/Add-faq.css" />
+    <script src="../resource/UI Library/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
+    <script src="../resource/js/setting.js"></script>
+    <script src="../resource/UI Library/jquery-3.3.1.min.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</head>
+<body>
+<div class="container-fluid">
+<button class="custombutton btn btn-sm offset-4 ">Back</button>
+<div class="row offset-4">
+<div class="col-5 mt-3">
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Title</label>
+  <input type="email" class="customstyle form-control" id="exampleFormControlInput1">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlTextarea1" class="form-label">Paragraph</label>
+  <textarea class="customstyle form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+</div>
+
+<button class="custombutton btn btn-sm offset-10 mt-3">Save</button>
+
+</div>
+</div>
+</div>
+</body>
+</html>
