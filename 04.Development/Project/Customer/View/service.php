9e871892d95e7c7adff9bedf31d2756565ab7626
YePyieOo <yepyieoo96@gmail.com>
2022-05-28 23:43:10 +0630

    view profile finish

--- 04.Development/Project/Customer/View/service.php
+++ 04.Development/Project/Customer/View/service.php
@@ -15,6 +15,59 @@
 
 <body>
 
+   <div class="container-fluid header d-flex justify-content-between align-items-center p-4 bg-light">
+      <h3 class="ms-5">SERVICES</h3>
+      <div class="me-5">
+         <b>Home</b> -> Services
+      </div>
+   </div>
+
+
+   <section class="container mt-3">
+      <div class="row">
+         <div class="col-12">
+            <h3 class="font-color-primary">Our Services</h3>
+            <hr>
+         </div>
+      </div>
+
+
+      <div class="row">
+         <div class="col-12">
+            <h5 class="font-color-primary">Store pickup & Delivery</h5>
+            <br>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam incidunt expedita quidem consectetur? Fuga natus voluptatum, laudantium quibusdam placeat est, odio velit rem necessitatibus assumenda nemo tenetur saepe blanditiis ex.
+               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam incidunt expedita quidem consectetur? Fuga natus voluptatum, laudantium quibusdam placeat est, odio velit rem necessitatibus assumenda nemo tenetur saepe blanditiis ex.
+               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam incidunt expedita quidem consectetur? Fuga natus
+            </p>
+
+
+
+            <h5 class="font-color-primary">Bookstore Policies </h5>
+            <br>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam incidunt expedita quidem consectetur? Fuga natus voluptatum, laudantium quibusdam placeat est, odio velit rem necessitatibus assumenda nemo tenetur saepe blanditiis ex.
+               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam incidunt expedita quidem consectetur? Fuga natus voluptatum, laudantium quibusdam placeat est, odio velit rem necessitatibus assumenda nemo tenetur saepe blanditiis ex.
+               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam incidunt expedita quidem consectetur? Fuga natus
+            </p>
+            <h5 class="font-color-primary">Delivery</h5>
+            <br>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam incidunt expedita quidem consectetur? Fuga natus voluptatum, laudantium quibusdam placeat est, odio velit rem necessitatibus assumenda nemo tenetur saepe blanditiis ex.
+               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam incidunt expedita quidem consectetur? Fuga natus voluptatum, laudantium quibusdam placeat est, odio velit rem necessitatibus assumenda nemo tenetur saepe blanditiis ex.
+               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam incidunt expedita quidem consectetur? Fuga natus
+            </p>
+            <h5 class="font-color-primary">Delivery Time</h5>
+            <br>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam incidunt expedita quidem consectetur? Fuga natus voluptatum, laudantium quibusdam placeat est, odio velit rem necessitatibus assumenda nemo tenetur saepe blanditiis ex.
+               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam incidunt expedita quidem consectetur? Fuga natus voluptatum, laudantium quibusdam placeat est, odio velit rem necessitatibus assumenda nemo tenetur saepe blanditiis ex.
+               Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam incidunt expedita quidem consectetur? Fuga natus
+            </p>
+         </div>
+      </div>
+
+
+   </section>
+
+
    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
    <script src="../resource/UI Library/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
