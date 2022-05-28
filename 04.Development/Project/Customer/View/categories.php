1ba5d4b79426af1a5ea3ca80bdd42a207ead1459
YePyieOo <yepyieoo96@gmail.com>
2022-05-28 19:18:52 +0630

    hello

--- 04.Development/Project/Customer/View/categories.php
+++ 04.Development/Project/Customer/View/categories.php
@@ -76,49 +76,49 @@
    </nav>
    <!-- nav -->
    <div class="container-fluid header d-flex justify-content-between align-items-center p-4">
-            <h3>Language</h3>
-            <div>
-                <b>Home</b> -> Language
-            </div>
-        </div>
-    </div>
-    <div class="container-fluid header  d-flex justify-content-between align-items-center ">
-        <div class="container">
-            <div class="row g-0">
-                <?php for ($i = 0; $i < 12; $i++) : ?>
-                    <div class="col-12 col-md-6 col-lg-3 text-light header d-flex justify-content-between align-items-center">
-                        <div class="my-card">
-                            <div class="my-card-img">
-                                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXg49v2amwPoDctZcQClmNEYTIlAHx_0SbHQ&usqp=CAU" alt="">
-                            </div>
-                            <div class="my-card-info">
-                                <h5>Book Name</h5>
-                                <div class="mb-2">
-                                    <span style="font-size:x-small">
-                                        <?php for ($s = 0; $s < 5; $s++) : ?>
-                                            <?php $star = 3; ?>
-                                            <?php if ($star > $s) : ?>
-                                                <i class="fa-solid fa-star"></i>
-                                            <?php else : ?>
-                                                <i class="fa-regular fa-star"></i>
-                                            <?php endif ?>
-                                        <?php endfor ?>
-                                    </span><br>
-                                    <span style="font-size:x-small">Author Name</span>
-                                </div>
-                                <p style="font-weight:500 ;">3500 MMK</p>
-                                <button class="btn btn-primary card-button px-1 rounded-1 me-1"><i class="fa-solid fa-cart-shopping me-1"></i>add
-                                    to
-                                    cart</button>
-                                <span class="btn btn-primary card-button"><i class="fa-solid fa-eye"></i> 4</span>
-                            </div>
+      <h3>Language</h3>
+      <div>
+         <b>Home</b> -> Language
+      </div>
+   </div>
+   </div>
+   <div class="container-fluid header  d-flex justify-content-between align-items-center ">
+      <div class="container">
+         <div class="row g-0">
+            <?php for ($i = 0; $i < 12; $i++) : ?>
+               <div class="col-12 col-md-6 col-lg-3 text-light header d-flex justify-content-between align-items-center">
+                  <div class="my-card">
+                     <div class="my-card-img">
+                        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXg49v2amwPoDctZcQClmNEYTIlAHx_0SbHQ&usqp=CAU" alt="">
+                     </div>
+                     <div class="my-card-info">
+                        <h5>Book Name</h5>
+                        <div class="mb-2">
+                           <span style="font-size:x-small">
+                              <?php for ($s = 0; $s < 5; $s++) : ?>
+                                 <?php $star = 3; ?>
+                                 <?php if ($star > $s) : ?>
+                                    <i class="fa-solid fa-star"></i>
+                                 <?php else : ?>
+                                    <i class="fa-regular fa-star"></i>
+                                 <?php endif ?>
+                              <?php endfor ?>
+                           </span><br>
+                           <span style="font-size:x-small">Author Name</span>
                         </div>
-                    </div>
-                <?php endfor ?>
-            </div>
-        </div>
-    </div>
-    <footer id="footer" class="container-fluid commom-bg">
+                        <p style="font-weight:500 ;">3500 MMK</p>
+                        <button class="btn btn-primary card-button px-1 rounded-1 me-1"><i class="fa-solid fa-cart-shopping me-1"></i>add
+                           to
+                           cart</button>
+                        <span class="btn btn-primary card-button"><i class="fa-solid fa-eye"></i> 4</span>
+                     </div>
+                  </div>
+               </div>
+            <?php endfor ?>
+         </div>
+      </div>
+   </div>
+   <footer id="footer" class="container-fluid commom-bg">
       <div class="row">
          <div class="col-12 col-lg-4 ps-5 pt-4">
             <div class="">
---
+++ 04.Development/Project/Customer/View/search.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Search</title>
+   <link rel="stylesheet" href="../resource/UI Library/bootstrap-5.0.2-dist/css/bootstrap.min.css" />
+   <link rel="stylesheet" href="../resource/css/style.css" />
+   <link rel="stylesheet" href="../resource/css/search.css" />
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css">
+   <link rel="stylesheet" href="../resource/UI Library/fontawesome-free/css/all.min.css">
+</head>
+
+<body>
+
+
+   <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+   <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+   <script src="../resource/UI Library/bootstrap-5.0.2-dist/js/bootstrap.min.js"></script>
+   <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+   <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+   <script src="../resource/UI Library/fontawesome-free/js/all.min.js"></script>
+</body>
+
+</html>
