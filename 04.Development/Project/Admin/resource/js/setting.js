d5c56dd1844ba43ca27693eca151c082a9a0727b
DESKTOP-KK6PVSK\user <nyimyomwa9@gmail.com>
2022-05-29 16:00:01 +0630

    Add Setting page

---
+++ 04.Development/Project/Admin/resource/js/setting.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+    $(".changemaster" ).click(function() {
+        if($(this).text()=="App Master"){
+        $(".stm").addClass("closemaster");
+        $(".lgm").addClass("closemaster")
+        $(".custom").removeClass("closemaster")
+        $(".custom").addClass("viewmaster");
+        
+       
+    }else if($(this).text()=="Site Master"){
+        $(".custom").addClass("closemaster");
+        $(".lgm").addClass("closemaster")
+        $(".stm").removeClass("closemaster")
+        $(".stm").addClass("viewmaster");
+    }else if($(this).text()=="Login Master"){
+        $(".custom").addClass("closemaster");
+        $(".stm").addClass("closemaster")
+        $(".lgm").removeClass("closemaster")
+        $(".lgm").addClass("viewmaster");
+    }
+       });
+});
