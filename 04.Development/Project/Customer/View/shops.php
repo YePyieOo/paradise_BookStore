cd0d753062460de9f85fb026311dda013130279c
nannaychilin95@gmail.com <nannaychilin95@gmail.com>
2022-05-30 18:21:57 +0630

    shops

--- 04.Development/Project/Customer/View/shops.php
+++ 04.Development/Project/Customer/View/shops.php
@@ -5,7 +5,7 @@
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>Homepage</title>
+   <title>Shops</title>
    <link rel="stylesheet" href="../resource/UI Library/bootstrap-5.0.2-dist/css/bootstrap.min.css" />
    <link rel="stylesheet" href="../resource/css/style.css" />
    <link rel="stylesheet" href="../resource/css/homepage.css" />
@@ -75,16 +75,16 @@
       </div>
    </nav>
    <!-- nav -->
-   <div class="container header d-flex justify-content-between align-items-center p-4">
+   <div class="container text-primary header d-flex justify-content-between align-items-center p-4">
             <h3>Shops</h3>
             <div>
-                <b>Home</b> -> shop
+                <b>Home</b> -> Shop
             </div>
         </div>
         <div class="container d-flex justify-content-between align-items-center p-4 mx-6 col-12 col-md-12 col-lg-4">
-        <button class="btn btn-primary text-light ">Yangon</button>
-        <button class="btn btn-primary text-light">Mandalay</button>
-        <button class="btn btn-primary text-light">Naypyidaw</button>
+        <button class="btn text-primary border border-primary">Yangon</button>
+        <button class="btn text-primary border border-primary">Mandalay</button>
+        <button class="btn text-primary border border-primary">Naypyidaw</button>
         </div>
     </div>
     <div class="container-fluid">
@@ -92,13 +92,13 @@
             <div class="row g-0">
                 <?php for ($i = 0; $i < 3; $i++) : ?>
                     <div class="col-12 col-md-12 col-lg-4">
-                        <div class="my-card m-5">
+                        <div class="my-card m-5 border border-primary rounded-2">
                             <div class="my-card-img">
                                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXg49v2amwPoDctZcQClmNEYTIlAHx_0SbHQ&usqp=CAU" alt="">
-                                <h6><b> Yar Pyae Book Shop</b></h6>
-                                <div class="shop_info">U Tun Lin Chan,Hlae Tan ,Kamayout,Yangon</div>
-                                <div class="shop_info">097665577,0876567899</div>
-                                <div class="shop_info">www.yarpyae.com</div>
+                                <h6 class="text-primary mt-4"><b> Yar Pyae Book Shop</b></h6>
+                                <div class="text-primary m-2 shop_info">U Tun Lin Chan,Hlae Tan ,Kamayout,Yangon</div>
+                                <div class="text-primary m-2 shop_info">097665577,0876567899</div>
+                                <div class="text-primary m-2 shop_info">www.yarpyae.com</div>
                             </div>
                         </div>
                     </div>
